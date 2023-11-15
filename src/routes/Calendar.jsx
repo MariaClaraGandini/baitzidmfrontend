66149087146afc04cacc488c60f2ab93da1f8de9
MariaClaraGandini <79529901+MariaClaraGandini@users.noreply.github.com>
2023-11-15 11:05:23 -0300

    ajustes

--- src/routes/Calendar.jsx
+++ src/routes/Calendar.jsx
@@ -27,13 +27,7 @@ function Calendar() {
   });
 
   const [modalOpen, setModalOpen] = useState(false);
-  // const [eventName, setEventName] = useState('');
-  // const [eventDate, setEventDate] = useState('');
-  // const [eventHour, setEventHour] = useState('');
-  // const [eventDescription, setEventDescription] = useState('');
-  // const [eventLocal, setEventLocal] = useState('');
   const [events, setEvents] = useState([]);
-  const [event, setEvent] = useState([]);
 
 
   const fetchEvents = async () => {
@@ -46,7 +40,6 @@ function Calendar() {
     }
   };
 
-// Usa método GET quando carrega a tela
   useEffect(() => {
     fetchEvents();
   }, []);
@@ -54,14 +47,14 @@ function Calendar() {
   const handleOpenModal = () => {
     setModalOpen(true);
 
-    if(modalOpen==false){
-      reset(); 
+    if (modalOpen == false) {
+      reset();
 
     }
-    
+
   };
 
-//Cria o evento
+  //Cria o evento
   const handleCreateEvent = async () => {
     try {
       const response = await fetch('http://localhost:5000/event/create', {
@@ -76,17 +69,17 @@ function Calendar() {
           description: watch('description'),
         }),
       });
-      
+
 
       if (response.ok) {
         // Lógica de sucesso
         setModalOpen(false); // Fechar o modal após o sucesso
         fetchEvents();
-       toast.success("Evento cadastrado com sucesso!");
-       reset(); // Limpa os valores do formulário após o envio bem-sucedido
+        toast.success("Evento cadastrado com sucesso!");
+        reset(); // Limpa os valores do formulário após o envio bem-sucedido
 
       } else {
-   
+
       }
     } catch (error) {
       console.error(error);
@@ -98,13 +91,11 @@ function Calendar() {
     try {
       const eventId = eventClickInfo.event.extendedProps._id;
       console.log(eventId)
-      
+
       const response = await fetch(`http://localhost:5000/event/getEvent/${eventId}`);
       if (response.ok) {
         const eventData = await response.json();
-        setEvent(eventData.event);
         setModalOpen(true);
-
         setValue("eventName", eventData.event.title);
         setValue("eventDate", eventData.event.start);
         setValue("eventHour", ""); // Talvez você precise ajustar isso dependendo do formato da hora
@@ -112,12 +103,13 @@ function Calendar() {
         setValue("eventDescription", eventData.event.description);
       } else {
         // Lógica de erro ao obter detalhes do evento
-        console.error("Erro na resposta do servidor:", response.status, response.statusText);      }
+        console.error("Erro na resposta do servidor:", response.status, response.statusText);
+      }
     } catch (error) {
       console.error(error);
     }
   };
-  
+
 
   return (
     <div className="container">
@@ -132,70 +124,70 @@ function Calendar() {
         locale={ptBrLocale}
         events={events
         }
-        eventClick={handleEventClick} 
+        eventClick={handleEventClick}
         customButtons={{
           modal: {
             text: 'Add Event',
-            click:handleOpenModal,
+            click: handleOpenModal,
           },
         }}
       />
 
-             {/* Botão para abrir o modal */}
-        <ToastContainer />
-{/* Renderiza o componente Modal com base no estado modalOpen */}
-<Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
-<TextField
-        margin="normal"
-        required
-        fullWidth
-        id="eventname"
-        label="Evento"
-        name="eventname"
-        {...register('eventname', { required: 'Nome do evento é obrigatório' })}
-        value={watch('eventname')}  // Ajuste para usar 'eventname' em vez de 'hour'
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-{formState.errors?.eventname && (
-  <p>{formState.errors.eventname.message}</p>
-)}
-<Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-    <TextField
-      margin="normal"
-      required
-      fullWidth
-      id="data"
-      label="Data"
-      name="data"
-      type="date"  // Defina o tipo como "date" para obter um campo de entrada de data
-      {...register('data', { required: 'Data do evento é obrigatório' })}
-      value={watch('data')}  // Ajuste para usar 'eventname' em vez de 'hour'
-      InputLabelProps={{
-        shrink: true,
-      }}
-    />
+      {/* Botão para abrir o modal */}
+      <ToastContainer />
+      {/* Renderiza o componente Modal com base no estado modalOpen */}
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="eventname"
+          label="Evento"
+          name="eventname"
+          {...register('eventname', { required: 'Nome do evento é obrigatório' })}
+          value={watch('eventname')}  // Ajuste para usar 'eventname' em vez de 'hour'
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        {formState.errors?.eventname && (
+          <p>{formState.errors.eventname.message}</p>
+        )}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="data"
+              label="Data"
+              name="data"
+              type="date"  // Defina o tipo como "date" para obter um campo de entrada de data
+              {...register('data', { required: 'Data do evento é obrigatório' })}
+              value={watch('data')}  // Ajuste para usar 'eventname' em vez de 'hour'
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
 
-      </Grid>
-      <Grid item xs={12} sm={6}>
+          </Grid>
+          <Grid item xs={12} sm={6}>
 
-    
-<TextField
-  margin="normal"
-  required
-  fullWidth
-  id="hour"
-  label="Hora"
-  name="hour"
-  type="time"
-  {...register('hour', { required: 'Horário do evento é obrigatório' })}
-  value={watch('hour')}  // Ajuste para usar 'eventname' em vez de 'hour'
-/>
-      </Grid>
-      </Grid>
-      <TextField
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="hour"
+              label="Hora"
+              name="hour"
+              type="time"
+              {...register('hour', { required: 'Horário do evento é obrigatório' })}
+              value={watch('hour')}  // Ajuste para usar 'eventname' em vez de 'hour'
+            />
+          </Grid>
+        </Grid>
+        <TextField
           margin="normal"
           required
           fullWidth
@@ -206,42 +198,42 @@ function Calendar() {
           value={watch('local')}  // Ajuste para usar 'eventname' em vez de 'hour'
         />
         <TextField
-      margin="normal"
-      required
-      fullWidth
-      id="textarea"
-      label="Descrição"
-      name="description"
-      multiline  // Indica que este é um campo de texto de área
-      rows={4}  
-      {...register('description')}
-      value={watch('description')}  // Ajuste para usar 'eventname' em vez de 'hour'
-      InputLabelProps={{
-        shrink: true,
-      }} // Define o número inicial de linhas visíveis
-    />
+          margin="normal"
+          required
+          fullWidth
+          id="textarea"
+          label="Descrição"
+          name="description"
+          multiline  // Indica que este é um campo de texto de área
+          rows={4}
+          {...register('description')}
+          value={watch('description')}  // Ajuste para usar 'eventname' em vez de 'hour'
+          InputLabelProps={{
+            shrink: true,
+          }} // Define o número inicial de linhas visíveis
+        />
 
-<Button
-        type="submit"
-        onClick={handleSubmit(handleCreateEvent)}
-        fullWidth
-        variant="contained"
-        sx={{
-          mt: 3,
-          mb: 2,
-          backgroundColor: '#c0623c',
-          ':hover': {
-            backgroundColor: '#a5522d',
-          },
-          ':active': {
-            backgroundColor: '#8f4324', // Cor para quando o botão estiver ativo
-          },
-        }}
-      >
-        Enviar
-      </Button>
-</Modal>
-       
+        <Button
+          type="submit"
+          onClick={handleSubmit(handleCreateEvent)}
+          fullWidth
+          variant="contained"
+          sx={{
+            mt: 3,
+            mb: 2,
+            backgroundColor: '#c0623c',
+            ':hover': {
+              backgroundColor: '#a5522d',
+            },
+            ':active': {
+              backgroundColor: '#8f4324', // Cor para quando o botão estiver ativo
+            },
+          }}
+        >
+          Enviar
+        </Button>
+      </Modal>
+
     </div>
   );
 }
